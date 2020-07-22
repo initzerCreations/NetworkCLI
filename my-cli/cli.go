1d7d3bfcda51bed6aa4834257bbc143cb15d2fb5
Samuel Santiago Huesca Briceno <s.s.huesca@udc.es>
2020-07-22 15:51:47 +0200

    MX records lookup command added

--- my-cli/cli.go
+++ my-cli/cli.go
@@ -37,6 +37,54 @@ func main() {
 				return nil
 			},
 		},
+		{
+			Name:  "ip",
+			Usage: "Looks up the IP addresses for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ip, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(ip); i++ {
+					fmt.Println(ip[i])
+				}
+
+				return nil
+			},
+		},
+		{
+			Name:  "cname",
+			Usage: "Looks up the CNAME for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				cname, err := net.LookupCNAME(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				fmt.Println(cname)
+				return nil
+			},
+		},
+		{
+			Name:  "mx",
+			Usage: "Looks up for the MX Records for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				mx, err := net.LookupMX(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(mx); i++ {
+					fmt.Println(mx[i].Host, mx[i].Pref)
+				}
+
+				return nil
+			},
+		},
 	}
 	err := app.Run(os.Args)
 	if err != nil {
